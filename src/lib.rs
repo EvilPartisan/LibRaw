b5321cb499e16a49e6f168ba290e2693a1cb371d
Stanislav Petrov <evilpartisan@gmail.com>
2026-01-20 02:16:31 +0300

    Монохромная улиточка (обрезанная)

--- src/lib.rs
+++ src/lib.rs
@@ -2,46 +2,141 @@
 #[allow(non_snake_case)]
 #[allow(non_upper_case_globals)]
 #[allow(non_camel_case_types)]
-pub mod libraw {
-    include!("../bindings.rs");
+pub mod libraw_sys {
+    include!("libraw-sys.rs");
 }
 mod tests;
 
-use errno::{Errno, errno, set_errno};
 use std::ffi::{CStr, CString};
-use std::fmt::Error;
-use std::ptr::{null, null_mut};
+use std::slice;
 use std::str::FromStr;
 
-use libraw::libraw_data_t as LibRaw;
-use libraw::*;
+use crate::libraw_sys::*;
 
-impl LibRaw {
+#[derive(Debug)]
+pub struct Libraw<'a> {
+    data: *mut libraw_data_t,
+    rawdata: Rawdata<'a>,
+    sizes: ImageSizes<'a>,
+}
+#[derive(Debug)]
+struct Rawdata<'a> {
+    raw_image: Option<&'a [u16]>,
+}
+#[derive(Debug)]
+struct ImageSizes<'a> {
+    pub raw_height: Option<&'a u16>,
+    pub raw_width: Option<&'a u16>,
+}
+
+// pub struct libraw_image_sizes_t {
+//     pub raw_height: ushort,
+//     pub raw_width: ushort,
+//     pub height: ushort,
+//     pub width: ushort,
+//     pub top_margin: ushort,
+//     pub left_margin: ushort,
+//     pub iheight: ushort,
+//     pub iwidth: ushort,
+//     pub raw_pitch: ::std::os::raw::c_uint,
+//     pub pixel_aspect: f64,
+//     pub flip: ::std::os::raw::c_int,
+//     pub mask: [[::std::os::raw::c_int; 4usize]; 8usize],
+//     pub raw_aspect: ushort,
+//     pub raw_inset_crops: [libraw_raw_inset_crop_t; 2usize],
+// }
+
+impl Libraw<'_> {
     pub fn new(flags: u32) -> Option<Self> {
-        let lr_data = unsafe { libraw::libraw_init(flags) };
-        if lr_data == null_mut() {
+        let lr_data = unsafe { libraw_init(flags) };
+
+        if lr_data.is_null() {
             return None;
         }
-        return Some(unsafe { *lr_data });
+
+        Some(Libraw {
+            data: lr_data,
+            rawdata: Rawdata { raw_image: None },
+            sizes: ImageSizes {
+                raw_height: None,
+                raw_width: None,
+            },
+        })
     }
+
     pub fn close(&mut self) {
-        unsafe { libraw_close(self) };
+        unsafe { libraw_close(self.data) };
+    }
+
+    pub fn recycle(&self) {
+        unsafe { libraw_recycle(self.data) };
     }
 
     //int LibRaw::open_file(const char *filename[,INT64 bigfile_size])
 
-    pub fn open_file(&mut self, filename: &str) -> Result<&Self, i32> {
-        let a = unsafe { libraw_open_file(self, CString::from_str(filename).unwrap().as_ptr()) };
-        //println!("{:#?}", errno);
+    pub fn unpack(&mut self) -> Result<&Self, LibRaw_errors> {
+        let unpkg_res = unsafe { libraw_unpack(self.data) };
 
-        println!("============Error {:#?}", self.strerror(a));
-        // if a != 0 {
-        //     return Err(a);
-        // };
-        return Ok(self);
+        if unpkg_res != LibRaw_errors_LIBRAW_SUCCESS {
+            return Err(unpkg_res);
+        }
+
+        let raw_height = unsafe { &(*self.data).rawdata.sizes.raw_height };
+        let raw_width = unsafe { &(*self.data).rawdata.sizes.raw_width };
+
+        self.sizes.raw_width = Some(raw_width);
+        self.sizes.raw_height = Some(raw_height);
+
+        let raw_image_c = unsafe {
+            slice::from_raw_parts(
+                (*self.data).rawdata.raw_image,
+                *raw_height as usize * *raw_width as usize,
+            )
+        };
+
+        self.rawdata.raw_image = Some(raw_image_c);
+
+        Ok(self)
     }
 
-    pub fn strerror(&self, e: ::std::os::raw::c_int) -> &'static str {
-        unsafe { CStr::from_ptr(libraw_strerror(e as i32)).to_str().unwrap() }
+    pub fn open_file(&mut self, filename: &str) -> Result<&Self, i32> {
+        let a =
+            unsafe { libraw_open_file(self.data, CString::from_str(filename).unwrap().as_ptr()) };
+
+        if a != LibRaw_errors_LIBRAW_SUCCESS {
+            return Err(a);
+        }
+
+        Ok(self)
+    }
+
+    pub fn ppm_tiff_writer(&mut self, filename: String) -> Result<&Self, LibRaw_errors> {
+        unsafe {
+            let res =
+                libraw_dcraw_ppm_tiff_writer(self.data, CString::new(filename).unwrap().as_ptr());
+            if res != LibRaw_errors_LIBRAW_SUCCESS {
+                return Err(res);
+            }
+        }
+        Ok(self)
+    }
+
+    pub fn dcraw_process(&mut self) -> Result<&Self, LibRaw_errors> {
+        let e = unsafe { libraw_dcraw_process(self.data) };
+        if e == LibRaw_errors_LIBRAW_SUCCESS {
+            return Ok(self);
+        }
+        Err(e)
+    }
+
+    pub fn strerror(e: ::std::os::raw::c_int) -> &'static str {
+        unsafe { CStr::from_ptr(libraw_strerror(e)).to_str().unwrap() }
+    }
+}
+
+impl Drop for Libraw<'_> {
+    fn drop(&mut self) {
+        self.recycle();
+        self.close();
     }
 }
--- src/tests.rs
+++ src/tests.rs
@@ -1,11 +1,67 @@
 #[cfg(test)]
 mod tests {
-    use crate::*;
+
+    use std::{
+        fs::File,
+        io::{BufWriter, Write},
+    };
+
+    use crate::{Libraw, libraw_sys::strerror};
 
     #[test]
     fn init_and_close() {
-        let mut lr_data = LibRaw::new(0).unwrap();
+        let mut lr_data = Libraw::new(0).unwrap();
+
         lr_data.open_file("./test_files/DSCF0730.RAF").unwrap();
-        lr_data.close();
+
+        let unpkg_res = lr_data.unpack();
+
+        match unpkg_res {
+            Err(e) => assert!(
+                false,
+                "{}",
+                unsafe { std::ffi::CStr::from_ptr(strerror(e)) }
+                    .to_str()
+                    .unwrap()
+            ),
+            Ok(_) => (),
+        }
+
+        let mut file = File::create("foo.pgm").unwrap();
+        let mut buf_file = BufWriter::new(file);
+
+        let _ = buf_file.write_all(b"P5\n");
+
+        let _ = buf_file.write_all(
+            lr_data
+                .sizes
+                .raw_width
+                .unwrap()
+                .clone()
+                .to_string()
+                .as_bytes(),
+        );
+        let _ = buf_file.write_all(b"\n");
+        let _ = buf_file.write_all(
+            lr_data
+                .sizes
+                .raw_height
+                .unwrap()
+                .clone()
+                .to_string()
+                .as_bytes(),
+        );
+
+        let _ = buf_file.write_all(b"\n255\n");
+
+        let img = lr_data.rawdata.raw_image.unwrap();
+
+        for el in img {
+            let _ = buf_file.write_all(&[el.to_le_bytes()[0]]);
+        }
+
+        let _ = buf_file.flush();
+
+        assert!(true);
     }
 }
