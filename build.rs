a74d59e7950503fb09c29d17a1b7ac6266d446a6
Stanislav Petrov <evilpartisan@gmail.com>
2026-01-20 02:15:25 +0300

    изменил bindgen

--- build.rs
+++ build.rs
@@ -8,9 +8,6 @@ fn main() {
 
     let bindings = bindgen::Builder::default()
         .header("libraw/libraw.h")
-        .allowlist_item(r#"(\w*libraw\w*)"#)
-        .allowlist_item(r#"(\w*LIBRAW\w*)"#)
-        .allowlist_item(r#"(\w*LibRaw\w*)"#)
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         .generate()
         .expect("Unable to generate bindings");
@@ -19,6 +16,6 @@ fn main() {
 
     bindings
         //  .write_to_file(out_path.join("bindings.rs"))
-        .write_to_file("bindings.rs")
+        .write_to_file("src/libraw-sys.rs")
         .expect("Couldn't write bindings!");
 }
